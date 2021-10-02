4f0a145b9ab7809f06a57bc0a4ebfacf9dc68e47
ManhTai01 <lemanhtaictbp@gmail.com>
2021-10-02 21:55:13 +0700

    cicd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ok cái gì cũng được
+          Deloy CICD
         </a>
         <h1>Con CAc</h1>
       </header>
