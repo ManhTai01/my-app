ac3e086a937220196e21cb3b7596292f5950a0a1
Quang Tran <nguyenkimthien2603@gmail.com>
2021-10-06 18:42:38 +0700

    slack2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test
+          Learn React slack
         </a>
         <h1>Con CAc</h1>
       </header>
