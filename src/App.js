f5bc465829cc85fb942607e3d7f8b57c5a59c6af
Lê Mạnh Tài <66947192+ManhTai01@users.noreply.github.com>
2021-10-06 16:15:35 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ok
+          Learn React test
         </a>
         <h1>Con CAc</h1>
       </header>
